940af1db96b6f6153b1407111972a29b497e6acd
aleee1 <alexfapper.2005@gmail.com>
2022-04-29 02:58:23 +0200

    rr

--- js/app.js
+++ js/app.js
@@ -1,29 +1,47 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-link');
-    const navLinks = document.querySelectorAll('.nav-link li');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-link");
+  const navLinks = document.querySelectorAll(".nav-link li");
 
+  burger.addEventListener("click", () => {
+    //alternanza navbar
+    nav.classList.toggle("nav-active");
 
-    burger.addEventListener('click', () => {
-        //alternanza navbar
-        nav.classList.toggle('nav-active');
+    //link animati
 
-
-        //link animati
-
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }
-
-
-        });
-
-        //animazione burger
-
-        burger.classList.toggle('toggle');
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
     });
-}
+
+    //animazione burger
+
+    burger.classList.toggle("toggle");
+  });
+};
 navSlide();
+
+$(".back-to-top").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $(".main-content").offset().top - 10,
+    },
+    1700
+  );
+});
+
+$(window).scroll(function () {
+  if (
+    $(window).scrollTop() - $(window).innerHeight() >
+    $(".main-content").height()
+  ) {
+    $(".back-to-top").removeClass("nascosto");
+  } else {
+    $(".back-to-top").addClass("nascosto");
+  }
+});
